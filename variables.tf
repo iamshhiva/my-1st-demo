148851e39eca9d9ef15df45c538eef6475eda4f9
iamshhiva <iamshivasu@gmail.com>
2025-11-28 15:01:21 +0530

    yes

--- variables.tf
+++ variables.tf
@@ -1,10 +1,10 @@
 variable "cidr_block_vpc" {
   description = "this is the code block"
-  defult = "10.0.0./16"
+  default = "10.0.0./16"
   type = string
 }
 variable "cidr_block_i_can_do" {
   description = "this is the code block"
-  defult = "10.0.0./24"
+  default = "10.0.0./24"
   type = string
 }
