3a7f3941b44477f426cfe777428aa273cce63d05
iamshhiva <iamshivasu@gmail.com>
2025-11-28 16:36:28 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,11 +1,11 @@
 resource "aws_vpc" "vpc1" {
-  cidr_block = cidr_block_vpc
+  cidr_block = var.cidr_block_vpc
   assign_generated_ipv6_cidr_block = true
 }
 
 resource "aws_subnet" "subnet1" {
   vpc_id     = aws_vpc.vpc1.id
-  cidr_block = cidr_block_i_can_do
+  cidr_block = var.cidr_block_i_can_do
 
   tags = {
     Name = "subnet1"
@@ -19,7 +19,7 @@ resource "aws_internet_gateway" "igw1" {
   }
 }
 resource "aws_egress_only_internet_gateway" "eigw1" {
-  vpc_id = aws_vpc.example.id
+  vpc_id = aws_vpc.vpc1.id
 
   tags = {
     Name = "eigw1"
