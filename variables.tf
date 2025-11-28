cc0210545bbcf53c6f7a742f84f5ea1eb683bc15
iamshhiva <iamshivasu@gmail.com>
2025-11-28 14:58:07 +0530

    againdone

--- variables.tf
+++ variables.tf
@@ -2,7 +2,6 @@ variable "cidr_block_vpc" {
   description = "this is the code block"
   defult = "10.0.0./16"
   type = string
-
 }
 variable "cidr_block_i_can_do" {
   description = "this is the code block"
