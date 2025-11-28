b91d959e733b5a5b7583f26cc2a441a034eb0bf9
iamshhiva <iamshivasu@gmail.com>
2025-11-28 16:39:53 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,10 +1,11 @@
 variable "cidr_block_vpc" {
   description = "this is the code block"
-  default = "10.0.0./16"
+  default = "10.0.0.0/16"
   type = string
 }
 variable "cidr_block_i_can_do" {
   description = "this is the code block"
-  default = "10.0.0./24"
+  default = "10.0.1.0/24"
   type = string
 }
+
