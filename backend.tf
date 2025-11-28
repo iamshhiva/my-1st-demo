7d4c07c4c7966a3c31516995afda36f66fb77ba5
iamshhiva <iamshivasu@gmail.com>
2025-11-28 16:18:51 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -2,11 +2,12 @@ terraform {
   required_version = ">= 1.0"
 
   backend "s3" {
-    bucket = "hub-new-bucket"
+    bucket = "hub-new-bucket1"
     key ="terraform.tfstate"
     region ="us-east-1"
     encrypt ="true"
   }
   
 }
+
 
